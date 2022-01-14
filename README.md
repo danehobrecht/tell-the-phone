aeee468e8257bb555fe7ec80ac641f744d07f86f
tmuq <76x5l22l@anonaddy.me>
2022-01-13 18:54:33 -0800

    modified:   README.md 	modified:   voice.py

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 COMPONENTS LIST
 
 Motherboard/Processor
-https://www.waveshare.com/product/rp2040-plus.htm
+https://www.adafruit.com/product/4884
 - RP2040-Plus
 - RAM is embedded in both the board and the processor
 - USB-C
@@ -25,3 +25,12 @@ Earpiece speaker
 Loud speaker
 
 Inputs
+
+NOTES & CONCEPTS
+
+Start out with the following rudimentary capabilites before considering more advanced ones:
+- Standard, cellular phone calls
+- Phonebook (contact storage)
+In short, base all of the initial features solely off of the provisions of a cell tower, minus the data.
+
+Try to use reminiscent language anywhere applicable in the operating system; i.e., "Phonebook" instead of "Contacts"
--- voice.py
+++ voice.py
@@ -1,8 +1,9 @@
 #!/user/bin/python3
 
+### Peforms cellular call of the passed parameter "number".
 def call(number):
 
-	### Code will vary with different GSM libraries
+	### Code will vary according to the calling library.
 
 	### Call
 	# print("Calling...")
@@ -18,23 +19,26 @@ def call(number):
 	# serialport.write("ATH\r")
 	# response = serialport.readlines(None)
 	# print(response)
+
 	print(number)
 
+### Formats and dials input for U.S. tellecommunication standards.
 def dial():
-	# User input
+	# Takes/cleans user input
 	user_input = input("+1 ").replace("-", "")
-	# Checks that the user input is a valid length
+	# Checks that the user input is of valid length
 	if len(user_input) != 10:
 		print("Invalid.")
 		dial()
+	# Appends U.S. area code
 	else:
-		# Appends U.S. area code
 		number = "1" + user_input
-		# Call "call"
 		call(number)
 
+### Main function.
 def main():
 	dial()
 
+### Initiates program.
 if __name__ == "__main__":
 	main()
