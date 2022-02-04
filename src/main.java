c04ae6b726e880da6d0e0c56e09f45f13e0ea195
tmuq <76x5l22l@anonaddy.me>
2022-02-03 19:22:55 -0800

    modified:   main.java

--- src/main.java
+++ src/main.java
@@ -4,18 +4,18 @@ public class Main {
 
 	public static void rejectWrite(String number) {
 		// Reject SMS
-		System.out.println("Rejected - " + number);
+		System.out.println("rejectWrite(" + number + ")");
 	}
 
 	public static void permitWrite(String number) {
 		// Permit SMS
-		System.out.println("Permitted - " + number);
+		System.out.println("permitWrite(" + number + ")");
 	}
 
 	public static void main(String[] args) {
 		// Feed number data from incoming SMS request
 		Scanner numberInput = new Scanner(System.in);
-		System.out.println("Enter a number: ");
+		System.out.print("Enter a number: ");
 		String number = numberInput.nextLine();
 		// Validate number
 		if (number.length() < 5 || number.length() > 6) {
